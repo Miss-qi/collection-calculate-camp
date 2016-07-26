945ddf0d73bd91a88d1ef11c7331c92fee241b8d
Miss-qi <1370322806@qq.com>
2016-07-26 20:52:18 +0800

    double_dimensional_array_to_one_2

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,11 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  let result = [];
+  collection.find(function (num) {
+    result = result.concat(num);
+  });
+  return Array.from(new Set(result));
 }
 
 module.exports = double_to_one;
