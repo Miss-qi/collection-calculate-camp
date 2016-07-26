58e967a28bc5670a50d17cdc6c0e580ac2a488d3
Miss-qi <1370322806@qq.com>
2016-07-26 20:40:07 +0800

    qiyanzi grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function grouping_count(collection) {
-
   //在这里写入代码
+  let arr = collection.reduce(function (cur,newVal) {
+    return (cur[newVal]++ || (cur[newVal] = 1),cur);
+  },{});
+  return arr;
 }
 
 module.exports = grouping_count;
