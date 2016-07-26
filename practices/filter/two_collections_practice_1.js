85b89844d7b1d975cedf28df124145767ac7c99c
Miss-qi <1370322806@qq.com>
2016-07-26 20:00:55 +0800

    qiyanzi two_collections_practice_1

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  let collection_c = [];
+  collection_a.find(function (numa) {
+    collection_b.find(function (numb) {
+      if (numa === numb){
+        collection_c.push(numa);
+      }
+    });
+  });
+  return collection_c;
 }
 
 module.exports = choose_common_elements;
