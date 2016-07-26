109746c5e5c3513e8d2c7298fe07f588076b15c6
Miss-qi <1370322806@qq.com>
2016-07-26 14:20:30 +0800

    qiyanzi get_intersection

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,6 +2,15 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  let collection_c = [];
+  collection_b.find(function (numb) {
+    collection_a.find(function (numa) {
+      if (numa === numb){
+        collection_c.push(numa);
+      }
+    });
+  });
+  return collection_c;
 }
 
 module.exports = get_intersection;
