fe781015e2c6ee22d48d052c7d198a8258aed69a
Miss-qi <1370322806@qq.com>
2016-07-26 20:24:56 +0800

    qiyanzi teo_collections_practice_3

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
   //在这里写入代码
+  let arr = [];
+  for(let i=0; i < collection_a.length; i++){
+    let flag = false;
+    for(let j=0; j < collection_b.length; j++){
+      if(collection_a[i] % collection_b[j] === 0)
+        flag = true;
+    }
+    if(flag)
+      arr.push(collection_a[i]);
+  }
+  return arr;
 }
 
 module.exports = choose_divisible_integer;
