a019396b59995fa94f1ad7ead8f049c78604a4f6
Miss-qi <1370322806@qq.com>
2016-07-26 14:18:55 +0800

    qiyanzi get_integer_interval

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,22 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  let collection = [];
+  if (number_a < number_b){
+    for (let i = number_a; i <= number_b; i++){
+      collection.push(i);
+    }
+  }
+  else if(number_a > number_b){
+    for (let i = number_a; i >= number_b; i--){
+      collection.push(i);
+    }
+  }
+  else {
+    collection.push(number_a);
+  }
+
+  return collection;
 }
 
 module.exports = get_integer_interval;
