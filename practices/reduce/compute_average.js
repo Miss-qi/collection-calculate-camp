e835617eb1c36cdb8b07a7f66c221c9286f119b3
Miss-qi <1370322806@qq.com>
2016-07-26 22:37:47 +0800

    compute_average

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,6 +2,9 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  return collection.reduce(function (cur, newVal) {
+      return cur + newVal;
+    }, 0) / collection.length;
 }
 
 module.exports = compute_average;
