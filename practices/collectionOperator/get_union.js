3d737fd846eade663a069f4976d13489bdbb3741
Miss-qi <1370322806@qq.com>
2016-07-26 14:21:01 +0800

    qiyanzi get_union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+ //  //在这里写入代码
+  let collection_c = collection_a.concat(collection_b);
+  return Array.from(new Set(collection_c));
 }
 
 module.exports = get_union;
