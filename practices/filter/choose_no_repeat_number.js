12595ab61b5609d27c889e7449d86c4f6fdcf151
Miss-qi <1370322806@qq.com>
2016-07-26 15:51:50 +0800

    qiyanzi choost_no_repeat_number

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
-  //在这里写入代码
+    return collection.filter(function(elem, pos, self) {
+// 如果没有重复项，返回true
+      return self.indexOf(elem, pos + 1) === -1;
+    });
 }
 
 module.exports = choose_no_repeat_number;
