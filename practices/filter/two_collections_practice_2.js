dae9c6432b9058a65cad8483f762fe8bcc845f22
Miss-qi <1370322806@qq.com>
2016-07-26 20:01:38 +0800

    qiyanzi two_collections_practice_2

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  let arr = [];
+  for(let i=0; i < collection_a.length; i++){
+    let flag = true;
+    for(let j=0; j < collection_b.length; j++){
+      if(collection_a[i] === collection_b[j])
+        flag = false;
+    }
+    if(flag)
+      arr.push(collection_a[i]);
+  }
+  return arr;
 }
 
 module.exports = choose_no_common_elements;
