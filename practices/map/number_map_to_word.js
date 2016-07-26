6b7456bb08315b688941a9bfa4545ccc70451124
Miss-qi <1370322806@qq.com>
2016-07-26 21:13:52 +0800

    number_map_to_word

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,8 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+var number_map_to_word = function (collection) {
+  return collection.map(function (num) {
+    return String.fromCharCode(64 + 32 + num);
+  });
 };
 
 module.exports = number_map_to_word;
