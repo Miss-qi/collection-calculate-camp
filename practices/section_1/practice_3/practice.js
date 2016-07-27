a8088d592dd8eaaa3eec79546273c4b561392c15
Miss-qi <1370322806@qq.com>
2016-07-27 10:54:16 +0800

    qiyanzi change practice3

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -2,13 +2,11 @@ function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
   let arr = [];
   collection_a.find(function (numa) {
-    for (let valued in object_b) {
-      object_b[valued].find(function (numb) {
-        if (numa === numb) {
-          arr.push(numa);
-        }
-      });
-    }
+    object_b.value.find(function (numb) {
+      if (numa === numb) {
+        arr.push(numa);
+      }
+    });
   });
   return arr;
 }
