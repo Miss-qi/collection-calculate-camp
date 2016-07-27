e37baf5d1fe1f5825836b069558df31c1c0cbc02
Miss-qi <1370322806@qq.com>
2016-07-27 10:11:17 +0800

    practice2

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,19 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  let collection_c = [];
+  let result = [];
+  collection_b.find(function (num) {
+    collection_c = collection_c.concat(num);
+  });
+  collection_c = Array.from(collection_c);
+  collection_a.find(function (numa) {
+    collection_c.find(function (numc) {
+      if (numa === numc) {
+        result.push(numa);
+      }
+    });
+  });
+  return result;
 }
 
 module.exports = collect_same_elements;
