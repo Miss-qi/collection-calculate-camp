3885ca2271c69010c00153efc8ef4455d333249d
Miss-qi <1370322806@qq.com>
2016-07-27 10:43:45 +0800

    qiyanzi practice4

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  let result = [];
+  collection_a.find(function (numa) {
+    for (let i in object_b) {
+      object_b[i].find(function (numb) {
+        if (numa.key === numb) {
+          result.push(numb);
+        }
+      });
+    }
+  });
+  return result;
 }
 
 module.exports = collect_same_elements;
